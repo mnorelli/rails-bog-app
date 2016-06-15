197a66e11f4df04ec1320c04d1abf1eac41649ee
Michael Norelli <mnorelli360@gmail.com>
2016-06-15 11:46:07 -0700

    adds seed file and Awesome Print gem for prettifying console

--- bog_app/db/seeds.rb
+++ bog_app/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Creature.create([{name: "Yoda", description: "900-year-old Jedi master. Lives in a swamp on Degoba. Steals food and giggles."},
+    {name: "Gollum", description: "originally known as Sméagol, was at first a Stoor, one of the three early Hobbit-types. Deformed and twisted in body and mind by the corruption of the Ring, his chief desire was to possess the very Ring that had enslaved him."},
+    {name: "Swamp Thing", description: "A humanoid mass of vegetable matter who fights to protect his swamp home, the environment in general, and humanity from various supernatural or terrorist threats."}
+  ])
